0709c783573dff5266b009cefc3c3101610d7c01
Ryan Tan <42328672+ryantanzr@users.noreply.github.com>
2021-03-25 18:03:05 +0800

    Adding models to admin page
    Learnt on how to add models to admin page, magic methods and formatting of HTML provided by Django

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'RyanT',
-        'title': 'Blog 1',
-        'content': 'Dummy Post',
-        'date_posted': '11th March, 2021'
-    },
-    {
-        'author': 'RyanT',
-        'title': 'Blog 2',
-        'content': 'Dummy Post2',
-        'date_posted': '11th March, 2021'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 

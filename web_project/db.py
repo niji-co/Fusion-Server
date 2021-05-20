f98ebd7bb221c170a943d7576ee68e2f424c8abc
Ryan Tan <42328672+ryantanzr@users.noreply.github.com>
2021-05-20 13:33:22 +0800

    FEAT: Mongo CRUD Operations Part 2
    -Setup and tested updating, insertion and deletion of subdocument fields.

--- web_project/db.py
+++ web_project/db.py
@@ -2,12 +2,10 @@ import pymongo
 
 from pymongo import MongoClient
 
-#tested
 def connect_to_mongo(connection_string):
     # Connect to a mongo client using the given connection string
     return MongoClient(connection_string)
 
-#tested
 def search_for_user(course_name, email):
     query_result = client["SDM"][course_name].aggregate([{'$match': { 'email': email } } ]) #returns a cursor
 
@@ -21,36 +19,41 @@ def search_for_user(course_name, email):
     user = query_result.next() #returns the document selected by the cursor
     return user
 
-#tested
 def update_user_info(field_to_change, new_info):
     update_result = collection.update_one(
         {'email' : user['email'] },
-        {"$set" : {field_to_change : new_info } }
-        , False)
+        {"$set" : {field_to_change : new_info } },
+        False)
     return update_result
 
-def update_project_info(field_to_change, new_info):
-    project_subfield_to_change = 'projects' + '.$.' + field_to_change #test this
-      
+#Note: If the field we are updating is not in the document, it will insert the new field for us.
+def update_project_info(project_name, field_to_change, new_info):
+    project_subfield_to_change = 'projects.$.' + field_to_change
+    
     update_result = collection.update_one(
-        {'$match': {'email' : user['email'], } },
-        {'$set' : { project_subfield_to_change : new_info }}
-        )
+        {
+            'email' : user['email'],
+            'projects.name' : project_name,
+        },
+        {
+            '$set' : { project_subfield_to_change : new_info }
+        })
+
     return update_result
 
 def insert_new_project(project_name):
     
-    insert_result = collection.update_one([
-        {'$match': {'email' : user['email'] } },
+    insert_result = collection.update_one(
+        {'email' : user['email'] },
         {'$push': { 'projects' : { 'name': project_name } } }
-        ])
+        )
     return insert_result
 
 def delete_project(project_name):
-    deletion_result = collection.update_one([
-            {'$match': {'email' : user['email'] } },
+    deletion_result = collection.update_one(
+            {'email' : user['email'] },
             {'$pull': { 'projects' : { 'name': project_name } } }
-            ])
+            )
     return deletion_result
 
 client = connect_to_mongo("mongodb+srv://MongoBongo:BongoBongo@djongoconnectiontest.nlqyc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")

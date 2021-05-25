374728fddcc25caf9b825b8f7a723413e772ad9c
Ryan Tan <42328672+ryantanzr@users.noreply.github.com>
2021-05-25 10:46:31 +0800

    Further formatting
    -Changed all " to '
    -indentation & spacings formatted

--- tests/test_insert_new_project.py
+++ tests/test_insert_new_project.py
@@ -4,19 +4,19 @@ import pymongo
 # import your functions/classes here
 from web_project.db import insert_new_project, search_for_user
 user_details = {
-    "course_name": "GDT",
-    "email": "ryantanzr.work@gmail.com",
-    "name": "Ryan Tan Zheng Rong"    
+    'course_name': 'GDT',
+    'email': 'ryantanzr.work@gmail.com',
+    'name': 'Ryan Tan Zheng Rong'    
 }
 
 project_details = {
-    "name": "Crusader-1",
-    "description": "kdgerhgwakfnjbhhqwuriqwfk"
+    'name': 'Crusader-1',
+    'description': 'kdgerhgwakfnjbhhqwuriqwfk'
 }
 
 def test_insert_new_project():
-    search_for_user(user_details["course_name"] ,user_details["email"])
-    insertion_result = insert_new_project(project_details["name"])
+    search_for_user(user_details['course_name'] ,user_details['email'])
+    insertion_result = insert_new_project(project_details['name'])
     
     assert insertion_result.acknowledged is True
-    assert insertion_result.modified_count == 1, "Project did not get inserted"
+    assert insertion_result.modified_count == 1, 'Project did not get inserted'
--- tests/test_update_user_info.py
+++ tests/test_update_user_info.py
@@ -4,14 +4,14 @@ import pymongo
 from web_project.db import update_user_info, search_for_user, user
 
 user_details = {
-    "course_name": "GDT",
-    "email": "ryantanzr.work@gmail.com",
-    "name": "Ryan Tan Zheng Rong"    
+    'course_name': 'GDT',
+    'email': 'ryantanzr.work@gmail.com',
+    'name': 'Ryan Tan Zheng Rong'    
 }
 
 def test_update_user_info():
-    search_for_user(user_details["course_name"] ,user_details["email"]) #init the program
-    update_operation = update_user_info("name", user_details["name"]) #update username
+    search_for_user(user_details['course_name'] ,user_details['email']) #init the program
+    update_operation = update_user_info('name', user_details['name']) #update username
     
     #Check for if the operation got acknowledged and if a data entry got modified
     assert update_operation.acknowledged is True
